aa128aab1a6e9b803af10d2e65d4b42ff8f2ea8d
HuijuanDai <dai00024@algonquinlive.com>
2018-02-09 09:12:52 -0500

    Lab 4

--- app/src/main/java/com/example/lucydai/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/lucydai/androidlabs/StartActivity.java
@@ -2,12 +2,94 @@ package com.example.lucydai.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
+    private static final String TAG = StartActivity.class.getSimpleName();
+    private Button button, button_chat;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 50:
+                Log.i(TAG, "Returned to StartActivity.onActivityResult");
+                break;
+        }
+
+        switch (resultCode) {
+            case Activity.RESULT_OK:
+                String messagePassed = data.getStringExtra("Response");
+                Toast.makeText(this, messagePassed, Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        button = findViewById(R.id.button_startPage);
+        button_chat = findViewById(R.id.button_startChart);
+        SharedPreferences sharedPreferences = getSharedPreferences("User info", Context.MODE_PRIVATE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemActivity.class);
+                startActivityForResult(intent, 50);
+            }
+        });
+
+        button_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(ACTIVITY_NAME, "User clicked Start Chat");
+                Intent intent = new Intent(StartActivity.this, ChatWindowActivity.class);
+                startActivityForResult(intent, 50);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
